37abf6e7ad31b8c552c3879173635fcd3016b87d
Szep Andor <andorbandi08@gmail.com>
2026-02-02 14:06:50 +0100

    A Math osztály átvéve

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double num = Math.PI;
+        System.out.println("PI értéke: " + num);
+        System.out.println(Math.abs(-25));
+        System.out.println(Math.pow(2, 4));
+        System.out.println(Math.sqrt(9));
+        
+        double szog = 1;
+        double rad = szog * Math.PI / 180;
+        System.out.println(Math.sin(rad));
+        System.out.println(Math.cos(rad));
+        System.out.println(Math.tan(rad));
+
+        System.out.println(Math.round(3.4));
+        System.out.println(Math.floor(3.7));
+        System.out.println(Math.ceil(3.3));
+        System.out.println(Math.random());
     }
 }
