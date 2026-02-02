dfe25f2137112da009e3fd5830eaaab37f9a1ef1
Szep Andor <andorbandi08@gmail.com>
2026-02-02 15:59:50 +0100

    Refaktrált Poker játék

--- src/Poker.java
+++ src/Poker.java
@@ -2,12 +2,11 @@ import java.util.Random;
 
 public class Poker {
     public static void play() {
-        machineThrow();
-        humanThrow();
-        // System.out.println("Kocka dobások: " + szam1 + " " + szam2 + " " + szam3 + " " + szam4 + " " + szam5);
+        throwDice("Gép");
+        throwDice("Ember");
     }
 
-    public static void machineThrow() {
+    private static void throwDice(String role) {
         Random random = new Random();
         int szam1 = random.nextInt(6) + 1;
         int szam2 = random.nextInt(6) + 1;
@@ -16,21 +15,7 @@ public class Poker {
         int szam5 = random.nextInt(6) + 1;
         System.out.printf(
             "%6s: %d %d %d %d %d\n",
-            "Gép",
-            szam1, szam2, szam3, szam4, szam5
-        );
-    }
-
-    public static void humanThrow() {
-        Random random = new Random();
-        int szam1 = random.nextInt(6) + 1;
-        int szam2 = random.nextInt(6) + 1;
-        int szam3 = random.nextInt(6) + 1;
-        int szam4 = random.nextInt(6) + 1;
-        int szam5 = random.nextInt(6) + 1;
-        System.out.printf(
-            "%6s: %d %d %d %d %d\n",
-            "Ember", 
+            role,
             szam1, szam2, szam3, szam4, szam5
         );
     }
