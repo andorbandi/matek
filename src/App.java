5d6adc20e25a8373ef159e1652ae29c887433a31
Szep Andor <andorbandi08@gmail.com>
2026-02-02 16:19:34 +0100

    Konvertálás áttekintése

--- src/App.java
+++ src/App.java
@@ -2,7 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Matek.csinald();
         // Veletlen.csinald();
-        Poker.play();
+        // Poker.play();
+        Konvetralas.csinal();   
     }
 }
 
---
+++ src/Konvetralas.java
@@ -0,0 +1,17 @@
+public class Konvetralas {
+    public static void csinal() {
+        String szoveg = "43";
+        int szam = Integer.parseInt(szoveg);
+
+        String szoveg2 = "43.25";
+        double szam2 = Double.parseDouble(szoveg2);
+
+        System.out.println(szam + " " + szam2);
+
+        int szam3 = 54;
+        String szoveg3 = String.valueOf(szam3);
+        double szam4 = 45.35;
+        String szoveg4 = String.valueOf(szam4);
+        System.out.println(szoveg3 + " " + szoveg4);
+    }
+}
